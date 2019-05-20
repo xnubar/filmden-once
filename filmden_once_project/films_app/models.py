d9eae544438d2eac7d89980f63cb51bcee20a69c
xnubar <nxalidova@std.qu.edu.az>
2019-05-20 16:00:21 +0400

    [+] models

--- filmden_once_project/films_app/models.py
+++ filmden_once_project/films_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from users_app.models import Member
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=150)
+    duration = models.CharField(max_length=20)
+    genre = models.CharField(max_length=255)
+    director = models.CharField(max_length=250)
+    review = models.CharField(max_length=800)
+    year = models.IntegerField()
+    country = models.CharField(max_length=250)
+    box_office = models.CharField(max_length=50)
+    member = models.ForeignKey(Member,  on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    poster = models.ImageField(upload_to='/static/images/')
+
+
+class Actor(models.Model):
+    fullname = models.CharField(max_length=250)
+    status = models.BooleanField(default=True)
+
+class Member_watchlist(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+class Film_Actors(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
--- filmden_once_project/users_app/models.py
+++ filmden_once_project/users_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth import User
 
-# Create your models here.
+class Member(User):
+    user_type = models.CharField(max_length=60)
+    profile_image = ImageField(upload_to=get_absoul, blank=True, null=True)
+    status = models.BooleanField(default=True)
