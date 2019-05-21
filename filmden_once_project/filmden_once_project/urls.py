1daa4967c4b8ca439e8fd83e2d8051b3df45235c
xnubar <nxalidova@std.qu.edu.az>
2019-05-21 12:33:41 +0400

    [+] news_app template

--- filmden_once_project/filmden_once_project/urls.py
+++ filmden_once_project/filmden_once_project/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('main_app.urls'))
+    path("", include('main_app.urls')),
+    path('news/', include('news_app.urls'))
 ]
