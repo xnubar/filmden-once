98289a1c7a08d5d963a4cebc1d13278140f4633b
FaiqRustamov <faiq.rustamov.98@mail.ru>
2019-05-22 19:48:35 +0400

    forgotpassword

--- filmden_once_project/users_app/urls.py
+++ filmden_once_project/users_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include 
 from users_app.views import *
-
+from django.conf.urls import url
+from users_app.login import views
 
 
 urtlpatterns = [
     path('accounts/', include ('django.contrib.auth.urls')),
+    path(r'^password/$', views.change_password, name='change_password'),
+
 ]
--- filmden_once_project/users_app/views.py
+++ filmden_once_project/users_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 
 
 def login(request):
@@ -7,6 +10,21 @@ def login(request):
 def index(request):
     return render(request, 'signup.html')
 
-
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
+       
 def create(request):
     email = request
